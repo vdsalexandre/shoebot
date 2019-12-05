0372b135c3c7287451407fd759c1ea2119159e40
AlexSuperCool <alexandre.vandensteen@gmail.com>
2019-12-05 11:50:42 +0100

    ajout d'un logger et refactoring des methodes Utils.java

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,29 +1,31 @@
 package utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Utils {
+    private static final Logger LOG = LogManager.getLogger(Utils.class);
     private static final String PROPERTIES_PATH = "config/application.properties";
     private static Properties p = null;
 
     public static String getProperty(String propertyKey) {
         try {
-            Properties properties = getPropertiesFile();
-            return properties.getProperty(propertyKey);
+            return getPropertiesFile().getProperty(propertyKey);
         } catch (IOException e) {
-            System.err.println("Properties file not found ...");
+            LOG.error("Properties file not found ...");
             return "";
         }
     }
 
     public static String[] getProperties(String propertyKey) {
         try {
-            Properties properties = getPropertiesFile();
-            return properties.getProperty(propertyKey).split(",");
+            return getPropertiesFile().getProperty(propertyKey).split(",");
         } catch (IOException e) {
-            System.err.println("Properties file not found ...");
+            LOG.error("Properties file not found ...");
             return null;
         }
     }
