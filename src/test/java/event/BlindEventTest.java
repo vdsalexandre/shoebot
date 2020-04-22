92e6c6c008aa32e73714842dcf96f26e108c1dce
vdsalex <alexandre.vandensteen@gmail.com>
2020-04-22 22:59:51 +0200

    add tests environnement

---
+++ src/test/java/event/BlindEventTest.java
@@ -0,0 +1,13 @@
+package event;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BlindEventTest {
+
+    @Test
+    void testSettings() {
+        assertThat(true).isTrue();
+    }
+}
